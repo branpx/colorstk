41dbccb73892c7eaef36d5785ffa692334db3bbc
Brandon Piatt <branpx@gmail.com>
2017-11-04 16:14:05 -0700

    Implement conversions on sRGB value change

--- colortk/lookup.py
+++ colortk/lookup.py
@@ -1,5 +1,12 @@
+from collections import OrderedDict
+
+import grapefruit
 from kivy.lang.builder import Builder
-from kivy.properties import (ListProperty,
+from kivy.properties import (DictProperty,
+                             ListProperty,
+                             NumericProperty,
+                             ObjectProperty,
+                             ReferenceListProperty,
                              StringProperty)
 from kivy.uix.screenmanager import Screen
 from kivy.uix.stacklayout import StackLayout
@@ -10,67 +17,73 @@ Builder.load_file('lookup.kv')
 
 
 class LookupScreen(Screen):
-    rgbhex = ListProperty(('#', 'ffffff'))
-    srgb = ListProperty(('sRGB', 1, 1, 1))
-    hsl = ListProperty(('HSL', 0, 0, 0))
-    hsv = ListProperty(('HSV', 0, 0, 0))
-    yiq = ListProperty(('YIQ', 0, 0, 0))
-    yuv = ListProperty(('YUV', 0, 0, 0))
-    ryb = ListProperty(('RYB', 0, 0, 0))
-    cie_xyz = ListProperty(('CIE-XYZ', 0, 0, 0))
-    cie_lab = ListProperty(('CIE-LAB', 0, 0, 0))
-    cmy = ListProperty(('CMY', 0, 0, 0))
-    cmyk = ListProperty(('CMYK', 0, 0, 0, 0))
+    rgbhex = ListProperty(('ffffff'))
+    srgb = ListProperty((1, 1, 1))
+    #hsl = ListProperty((0, 0, 0))
+    #hsv = ListProperty((0, 0, 0))
+    #yiq = ListProperty((0, 0, 0))
+    #yuv = ListProperty((0, 0, 0))
+    #cie_xyz = ListProperty((0, 0, 0))
+    #cie_lab = ListProperty((0, 0, 0))
+    #cmy = ListProperty((0, 0, 0))
+    #cmyk = ListProperty((0, 0, 0, 0))
     color_name = StringProperty(('<COLOR NAME>'))
+    #color_values = ObjectProperty(OrderedDict(sRGB=srgb,
+    #                                          HSL=[0, 0, 0],
+    #                                          HSV=[0, 0, 0],
+    #                                          YIQ=[0, 0, 0],
+    #                                          YUV=[0, 0, 0],
+    #                                          CIE_XYZ=[0, 0, 0],
+    #                                          CIE_LAB=[0, 0, 0],
+    #                                          CMY=[0, 0, 0],
+    #                                          CMYK=[0, 0, 0, 0]))
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        values = [self.srgb, self.hsl, self.hsv,
-                  self.yiq, self.yuv, self.ryb, self.cie_xyz,
-                  self.cie_lab, self.cmy, self.cmyk]
-        for value in values:
-            self.ids.value_stack.add_widget(ValueDisplay(value))
-
-    def update(self):
-        pass
+        self.color_values = OrderedDict(sRGB=self.srgb,
+                                        HSL=[0, 0, 0],
+                                        HSV=[0, 0, 0],
+                                        YIQ=[0, 0, 0],
+                                        YUV=[0, 0, 0],
+                                        CIE_XYZ=[0, 0, 0],
+                                        CIE_LAB=[0, 0, 0],
+                                        CMY=[0, 0, 0],
+                                        CMYK=[0, 0, 0, 0])
+        self.value_displays = []
+        for color_space, value in self.color_values.items():
+            value_display = ValueDisplay(value, color_space)
+            self.ids.value_stack.add_widget(value_display)
+            self.value_displays.append(value_display)
 
     def on_srgb(self, instance, value):
-        pass
-
-    def on_hsl(self, instance, value):
-        pass
-
-    def on_hsv(self, instance, value):
-        pass
-
-    def on_yiq(self, instance, value):
-        pass
-
-    def on_yuv(self, instance, value):
-        pass
-
-    def on_ryb(self, instance, value):
-        pass
-
-    def on_cie_xyz(self, instance, value):
-        pass
-
-    def on_cie_lab(self, instance, value):
-        pass
-
-    def on_cmy(self, instance, value):
-        pass
-
-    def on_cmyk(self, instance, value):
-        pass
+        self.color_values['HSL'][:] = grapefruit.rgb_to_hsl(*self.srgb)
+        self.color_values['HSV'][:] = grapefruit.rgb_to_hsv(*self.srgb)
+        self.color_values['YIQ'][:] = grapefruit.rgb_to_yiq(*self.srgb)
+        self.color_values['YUV'][:] = grapefruit.rgb_to_yuv(*self.srgb)
+        self.color_values['CIE_XYZ'][:] = grapefruit.rgb_to_xyz(*self.srgb)
+        self.color_values['CIE_LAB'][:] = grapefruit.xyz_to_lab(
+            *self.color_values['CIE_XYZ'])
+        self.color_values['CMY'][:] = grapefruit.rgb_to_cmy(*self.srgb)
+        self.color_values['CMYK'][:] = grapefruit.cmy_to_cmyk(
+            *self.color_values['CMY'])
+        for value_display in self.value_displays:
+            value_display.update_inputs()
 
 
 class ValueDisplay(StackLayout):
-    def __init__(self, value, **kwargs):
+    def __init__(self, value, color_space, **kwargs):
         super().__init__(**kwargs)
-        self.ids.value_label.text = value[0]
-        for num in range(1, len(value)):
-            self.add_widget(ValueInput(value, num))
+        self.value = value
+        self.ids.value_label.text = color_space
+        self.value_inputs = []
+        for index in range(len(value)):
+            value_input = ValueInput(value, index)
+            self.add_widget(value_input)
+            self.value_inputs.append(value_input)
+
+    def update_inputs(self):
+        for value_input in self.value_inputs:
+            value_input.text = str(value_input.value[value_input.index])
 
 
 class ValueInput(TextInput):
