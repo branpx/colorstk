d9d00a90dbad4491925bd00b56a1b4e2b895ded7
Brandon Piatt <branpx@gmail.com>
2017-11-10 18:18:12 -0800

    Add basic input validation

--- colortk/lookup.py
+++ colortk/lookup.py
@@ -2,10 +2,10 @@ from collections import OrderedDict
 
 import grapefruit
 from kivy.lang.builder import Builder
+from kivy.uix.gridlayout import GridLayout
 from kivy.properties import (ObjectProperty,
                              StringProperty)
 from kivy.uix.screenmanager import Screen
-from kivy.uix.stacklayout import StackLayout
 from kivy.uix.textinput import TextInput
 
 
@@ -26,11 +26,11 @@ class LookupScreen(Screen):
                     ('CMY', self.color.cmy), ('CMYK', self.color.cmyk)]
         self.color_values = OrderedDict(displays)
         for color_space, value in self.color_values.items():
-            self.ids.value_stack.add_widget(
+            self.ids.value_grid.add_widget(
                 ValueDisplay(self, color_space, value))
 
     def on_color(self, instance, color):
-        for value_display in self.ids.value_stack.children:
+        for value_display in self.ids.value_grid.children:
             if value_display.color_space == 'Hex':
                 value_display.value = [self.color.html]
             elif value_display.color_space == 'sRGB':
@@ -54,7 +54,7 @@ class LookupScreen(Screen):
             value_display.update_inputs()
 
 
-class ValueDisplay(StackLayout):
+class ValueDisplay(GridLayout):
     def __init__(self, lookup_screen, color_space, value, **kwargs):
         self.color_space = color_space
         self.lookup_screen = lookup_screen
@@ -97,6 +97,7 @@ class ValueDisplay(StackLayout):
             self.lookup_screen.color = grapefruit.Color.from_cmy(*self.value)
         elif self.color_space == 'CMYK':
             self.lookup_screen.color = grapefruit.Color.from_cmyk(*self.value)
+        print(self.lookup_screen.color.is_legal)
 
 
 class ValueInput(TextInput):
@@ -105,7 +106,7 @@ class ValueInput(TextInput):
         self.index = index
 
     def on_focus(self, instance, focused):
-        if (not focused and self.text and
+        if (not focused and self.text and self.valid_input() and
                 self.text != self.format_value(self.parent.value[self.index])):
             if self.parent.color_space == 'Hex':
                 self.parent.value = [self.text]
@@ -114,6 +115,36 @@ class ValueInput(TextInput):
             self.parent.update_color()
         else:
             self.text = self.format_value(self.parent.value[self.index])
+
+    def valid_input(self):
+        if self.parent.color_space == 'Hex':
+            if self.text.startswith('#'):
+                hex_value = self.text[1:]
+            else:
+                hex_value = self.text
+            if len(hex_value) == 3 or len(hex_value) == 6:
+                try:
+                    hex_value = int(hex_value, 16)
+                except ValueError:
+                    return False
+            else:
+                return False
+        elif self.parent.color_space == 'sRGB':
+            if not 0 <= float(self.text) <= 255:
+                return False
+        elif (self.parent.color_space == 'HSV' or
+              self.parent.color_space == 'HSL'):
+            if self.index == 0:
+                if not 0 <= float(self.text) <= 359:
+                    return False
+            else:
+                if not 0 <= float(self.text) <= 1:
+                    return False
+        elif (self.parent.color_space == 'CMY' or
+              self.parent.color_space == 'CMYK'):
+            if not 0 <= float(self.text) <= 1:
+                return False
+        return True
 
     @staticmethod
     def format_value(val, digits=3):
