db42fdaedc4beda2e69eae3d433633c7f5333ff9
Brandon Piatt <branpx@gmail.com>
2017-11-21 17:21:40 -0800

    Move action bar to root and add screen menu

--- colorstk/main.py
+++ colorstk/main.py
@@ -1,4 +1,8 @@
 from kivy.app import App
+from kivy.lang.builder import Builder
+from kivy.properties import BooleanProperty
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager
 
 import canvas
@@ -8,16 +12,40 @@ import picker
 import settings
 
 
-class ColorTKApp(App):
+Builder.load_file('main.kv')
+
+
+class RootWidget(FloatLayout):
+    pass
+
+
+class ScreenMenu(GridLayout):
+    toggled = BooleanProperty(False)
+
+    def on_toggled(self, instance, toggled):
+        if self.toggled:
+            self.disabled = False
+            self.opacity = 1
+        else:
+            self.disabled = True
+            self.opacity = 0
+
+    def on_touch_down(self, touch):
+        if not self.collide_point(*touch.pos):
+            self.toggled = False
+        super().on_touch_down(touch)
+
+
+class ColorsTKApp(App):
     def build(self):
-        screen_manager = ScreenManager()
-        screen_manager.add_widget(lookup.LookupScreen(name='lookup'))
-        screen_manager.add_widget(picker.PickerScreen(name='picker'))
-        screen_manager.add_widget(palettes.PalettesScreen(name='palettes'))
-        screen_manager.add_widget(canvas.CanvasScreen(name='canvas'))
-        screen_manager.add_widget(settings.SettingsScreen(name='settings'))
-        return screen_manager
+        root_widget = RootWidget()
+        root_widget.ids.screen_manager.add_widget(lookup.LookupScreen(name='lookup'))
+        root_widget.ids.screen_manager.add_widget(picker.PickerScreen(name='picker'))
+        root_widget.ids.screen_manager.add_widget(palettes.PalettesScreen(name='palettes'))
+        root_widget.ids.screen_manager.add_widget(canvas.CanvasScreen(name='canvas'))
+        root_widget.ids.screen_manager.add_widget(settings.SettingsScreen(name='settings'))
+        return root_widget
 
 
 if __name__ == '__main__':
-    ColorTKApp().run()
+    ColorsTKApp().run()
