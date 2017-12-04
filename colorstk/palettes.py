067b736f5242f4185698868ec1b4214d5039a774
Brandon Piatt <branpx@gmail.com>
2017-12-03 22:44:41 -0800

    Add color screen selection mode and deletion

--- colorstk/palettes.py
+++ colorstk/palettes.py
@@ -2,7 +2,8 @@ from kivy.app import App
 from kivy.clock import Clock
 from kivy.factory import Factory
 from kivy.lang.builder import Builder
-from kivy.properties import (ListProperty,
+from kivy.properties import (BooleanProperty,
+                             ListProperty,
                              ObjectProperty,
                              StringProperty)
 from kivy.storage.jsonstore import JsonStore
@@ -58,27 +59,56 @@ class PalettesScreen(KNSpaceBehavior, BoxLayout, Screen):
 
 
 class ColorsScreen(KNSpaceBehavior, BoxLayout, Screen):
+    mode = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(ColorsScreen, self).__init__(**kwargs)
+        self.mode = 'normal'
+        self.delete_button = ActionButton(
+            text='Delete', on_release=self.delete_color)
+
     def load_colors(self, palette):
+        self.palette = palette
         for color in palette.colors:
             self.ids.color_stack.add_widget(PaletteColor(color=color))
 
+    def on_mode(self, instance, mode):
+        action_previous = self.ids.action_previous
+        action_view = self.ids.action_view
+        if mode == 'normal':
+            action_previous.title = 'Colors'
+        elif mode == 'selection':
+            action_previous.title = 'Selection'
+            action_view.add_widget(self.delete_button)
+
     def on_leave(self):
+        self.palette = None
         self.ids.color_stack.clear_widgets()
+
+    def delete_color(self, button=None):
+        color_stack = self.ids.color_stack
+        for color_widget in color_stack.selected_nodes:
+            color_stack.remove_widget(color_widget)
+            self.palette.colors.remove(color_widget.color)
+        color_stack.clear_selection()
+        self.ids.action_view.remove_widget(self.delete_button)
+        self.mode = 'normal'
 
 
 class SelectableStack(CompoundSelectionBehavior, StackLayout):
     def select_node(self, node):
-        node.background_color = (0.4, 0.4, 0.4, 1)
+        node.selected = True
         return super(SelectableStack, self).select_node(node)
 
     def deselect_node(self, node):
-        node.background_color = (0.2, 0.2, 0.2, 1)
+        node.selected = False
         super(SelectableStack, self).deselect_node(node)
 
 
 class Palette(GridLayout):
     name = StringProperty()
     colors = ListProperty()
+    selected = BooleanProperty(False)
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
@@ -102,6 +132,12 @@ class Palette(GridLayout):
     def on_colors(self, instance, colors):
         knspace.palettes_screen.palettes.put(self.name, colors=self.colors)
 
+    def on_selected(self, instance, selected):
+        if selected:
+            self.background_color = (0.4, 0.4, 0.4, 1)
+        else:
+            self.background_color = (0.2, 0.2, 0.2, 1)
+
     def trigger_selection(self, dt):
         knspace.palettes_screen.mode = 'selection'
         self.parent.select_with_touch(self)
@@ -109,6 +145,28 @@ class Palette(GridLayout):
 
 class PaletteColor(Widget):
     color = ObjectProperty()
+    selected = BooleanProperty(False)
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            if knspace.colors_screen.mode == 'normal':
+                clock_event = Clock.schedule_once(self.trigger_selection, 1)
+                touch.ud['trigger_selection'] = clock_event
+            elif knspace.colors_screen.mode == 'selection':
+                self.parent.select_with_touch(self, touch)
+
+    def on_touch_up(self, touch):
+        Clock.unschedule(touch.ud.get('trigger_selection'))
+
+    def on_selected(self, instance, selected):
+        if selected:
+            self.border_color = (0.8, 0.8, 0.8, 1)
+        else:
+            self.border_color = (0.3, 0.3, 0.3, 1)
+
+    def trigger_selection(self, dt):
+        knspace.colors_screen.mode = 'selection'
+        self.parent.select_with_touch(self)
 
 
 class NewPalettePopup(Popup):
